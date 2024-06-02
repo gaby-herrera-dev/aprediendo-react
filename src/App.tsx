0976593798ab1472427aaccbb383390a5c34beeb
Gabriela Herrera <gaby-herrera-dev@outlook.es>
2024-06-01 18:51:38 -0600

    feat: agregue la función de calculadora para sumar dos números y mostrar el resultado

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 
-import { useState } from "react"
+import { Calcular } from "./components/Calcular"
 
 function App() {
-  const [contador, setContador  ] = useState<number>(0)
+  
 
-  function click(){
-    console.log("contador", contador)
-    setContador(contador + 1)
-
-  }
+  
   return (
     <>
-    <h2>contador</h2>
-    <button onClick={click}> suma 1</button>
-   <br/>
-   <h1>
-    {contador}
-   </h1>
+    <Calcular/>
     </>
   )
 }
--- src/components/Calcular.tsx
+++ src/components/Calcular.tsx
@@ -1,14 +1,37 @@
-export const PI = 3.1416
-export function Calcular (){
+import { useState } from 'react';
+
+export const PI = 3.1416;
+
+export function Calcular() {
+    const [num1, setNum1] = useState<number>(0);
+    const [num2, setNum2] = useState<number>(0);
+    const [resultado, setResultado] = useState<number>(0);
+
+    function handleSumar(){
+        const suma = num1 + num2;
+        setResultado(suma);
+    }
+    
     return (
-        <div className="Calculadora">
+        <div className="calculador">
             <h1>Calcular</h1>
 
+            <input
+                type="number"
+                placeholder="num 1"
+                value={num1} 
+                onChange={(e) => setNum1(Number(e.target.value))}
+            />
 
-            <input type="number" placeholder="num1"/>
-            <input type="number" placeholder="num2"/>
-            <button>sumar</button>
-            <p>Resultado es: 0 </p>
+            <input 
+                type="number" 
+                placeholder="num 2" 
+                value={num2} 
+                onChange={(e) => setNum2(Number(e.target.value))} 
+            />
+
+            <button onClick={handleSumar}>Sumar</button>
+            <p>Resultado es: {resultado}</p>
         </div>
-    )
+    );
 }
